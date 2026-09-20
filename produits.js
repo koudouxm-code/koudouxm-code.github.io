5160f038a97bb0bb68a3174dc2f4a8651a755c27
koudouxm-code <koudouxm@gmail.com>
2026-09-20 22:36:38 +0000

    Remove products from PRODUITS array
    Removed several product entries from the PRODUITS array.

--- produits.js
+++ produits.js
@@ -6,17 +6,9 @@
 // fond : couleur affichée tant que la photo n'est pas ajoutée
 const PRODUITS = [
   { nom: "Bijoux igbo", prix: "25 000 FCFA", categorie: "Bijoux",
-    image: "akan1.jpeg", page: "produits/bijoux-igbo.html",
+    image: " akan1.jpeg ", page: "produits/bijoux-igbo.html",
     vedette: true, fond: "#7C2A24" },
-  { nom: "Abodjé noir Or", prix: "15 000 FCFA", categorie: "Chaussures",
-    image: "images/abodje-noir-or.jpg", page: "produits/abodje-noir-or.html",
-    vedette: true, fond: "#3B2A1E" },
-  { nom: "Parure corail", prix: "XX 000 FCFA", categorie: "Bijoux",
-    image: "images/parure-corail.jpg", page: "produits/parure-corail.html",
-    vedette: true, fond: "#5A1F1B" },
-  { nom: "Chaussures noires", prix: "XX 000 FCFA", categorie: "Chaussures",
-    image: "images/chaussures-noires.jpg", page: "produits/chaussures-noires.html",
-    vedette: false, fond: "#2A2622" }
+  
 ];
 
 const CATEGORIES = ["Tous", "Chaussures", "Bijoux", "Tenues", "Divers"];
