d96e8371da854d9f476e06f58b1996c53cfaa728
koudouxm-code <koudouxm@gmail.com>
2026-09-20 22:39:02 +0000

    Clean up product definitions in produits.js
    Removed comments about product modifications and formatting.

--- produits.js
+++ produits.js
@@ -1,12 +1,7 @@
-// MODIFIE TES PRODUITS ICI
-// nom, prix, categorie (Chaussures, Bijoux, Tenues ou Divers)
-// image : le chemin de la photo dans ton repository
-// page : la page du produit vers laquelle le clic redirige
-// vedette : true = la photo défile dans la bannière du haut
-// fond : couleur affichée tant que la photo n'est pas ajoutée
+
 const PRODUITS = [
   { nom: "Bijoux igbo", prix: "25 000 FCFA", categorie: "Bijoux",
-    image: " akan1.jpeg ", page: "produits/bijoux-igbo.html",
+    image: "akan1.jpeg", page: "produits/bijoux-igbo.html",
     vedette: true, fond: "#7C2A24" },
   
 ];
